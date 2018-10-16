e141d1f5f6518961d4cca000624cb3cfb1c06b18
Dustin Moris Gorski <dustinmoris@gmail.com>
2018-10-17 00:12:29 +0100

    Updated Giraffe to v3 and Giraffe.Razor to v2
    Updated NuGet packages.

--- src/content/Razor/src/AppNamePlaceholder/Program.fs
+++ src/content/Razor/src/AppNamePlaceholder/Program.fs
@@ -18,7 +18,7 @@ open AppNamePlaceholder.Models
 let indexHandler (name : string) =
     let greetings = sprintf "Hello %s, from Giraffe!" name
     let model     = { Text = greetings }
-    razorHtmlView "Index" model
+    razorHtmlView "Index" (Some model) None
 
 let webApp =
     choose [
